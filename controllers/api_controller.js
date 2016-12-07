8f1ab184d9d62d552088b10fee1cb0e0174bfe11
Foo Fang Hau <cozyazure@hotmail.com>
2016-12-07 15:11:41 +0800

    change key to request params

--- controllers/api_controller.js
+++ controllers/api_controller.js
@@ -3,8 +3,15 @@
 var express= require('express');
 var router = express.Router();
 
-router.get('/object',(req,res,next)=>{
-    res.json({"key":"You know nothing"});
+router.get('/object/:key',(req,res,next)=>{
+    var key = req.params.key;
+    //TODO:perform db transaction here.  Mock for now
+    var response ={
+        "key":key,
+        "value":"RandomValue",
+        "TimeStamp":Date.now()
+    }
+    res.json(response);
 })
 
 module.exports = router;
